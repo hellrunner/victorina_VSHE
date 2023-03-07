a387b131b87ed53bfcebae97adabfe9baac48c80
hellrunner <037574@gmail.com>
2023-03-08 02:32:17 +0300

    try2

--- victorina.py
+++ victorina.py
@@ -20,20 +20,29 @@ def hi(): # функция приветствия к клиенту
 def game():
 
 
-    file = open("otchet.txt", "w")
+    file = open("otchet.txt", "w") # создаём файл
     score = 0
     print("-----------------------------------------------------------")
     for i in range(len(questions_answers)):
         Q = questions_answers[i]["Q"]
-        answers = "Варианты ответа на вопрос:\n"
+
+        answers = "Варианты ответа на вопрос:\n" # создаём список ответов
         for j in range((questions_answers[i]["Acounter"])):
             answers += str(j+1) +")" + str(questions_answers[i]["A" + str(j+1)]) + "  "
         print(Q)
         print(answers)
         file.write("Вопрос который был задан: " + Q + "\n" + answers + "\n")
         
-        user_A = str(input())
-        if questions_answers[i]["R_Acounter"]:
+        user_A = int(input())
+        R_Acounter_mass = []
+        for j in range((questions_answers[i]["R_Acounter"])):
+            R_Acounter_mass.append(questions_answers[i]["R_A" + str(j+1)])
+
+        if user_A in R_Acounter_mass:
+            score += 1
+        else:
+            pass
+    print(score)
             
         
     file.close()
